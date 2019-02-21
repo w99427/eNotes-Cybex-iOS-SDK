d041eeb9bfd8761bd0e831ffadb2b0734baea6f1
Victor Xu <victor@jiandeMacBook-Pro.local>
2019-02-21 18:57:56 +0800

    init enotes sdk for cybex ios

---
+++ eNotesFW/TLVBox.h
@@ -0,0 +1,26 @@
+//
+//  TLVBox.h
+//  ColdCornOC
+//
+//  Created by Victor Xu on 2018/7/3.
+//  Copyright © 2018年 Smiacter Yin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TLVBox : NSObject
+
+
+#define  Device_Certificate @"30"
+#define  BlockChain_PublicKey @"55"
+#define  SlavePrivateKey @"58"
+#define  KeyDeriveNonce  @"74"
+#define  KeyDeriveDigest @"75"
+#define  KeyDeriveSignature @"76"
+
+@property(nonatomic,strong)NSMutableDictionary *tlvDic;
+
+-(NSDictionary*)parse:(NSData*) tlvData;
+-(NSData*)serialize;
+-(void)putBytes:(NSData*) type andValue:(NSData*)value;
+@end
